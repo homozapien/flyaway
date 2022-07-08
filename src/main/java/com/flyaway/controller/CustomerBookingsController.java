7e7747aaf6652508dc8ac80af1bd0140fada00c9
homozapien <bakau.onafuwa@softgineer.com>
2022-07-08 10:59:41 +0100

    CustomerBookingController / update hibernate config file

---
+++ src/main/java/com/flyaway/controller/CustomerBookingsController.java
@@ -0,0 +1,40 @@
+package com.flyaway.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
+
+
+/**
+ * Servlet implementation class CustomerBookingsController
+ */
+public class CustomerBookingsController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public CustomerBookingsController() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
